a033ed2779898c6593c900e47ce76d209690be41
kez <kerrialbeckettnewham@gmail.com>
2025-04-20 16:48:17 +0200

    add avatar

---
+++ assets/vue/controllers/Identicon.vue
@@ -0,0 +1,23 @@
+<template>
+  <Avatar :name="kerrial-newham" variant="beam" :size="size" />
+</template>
+
+<script>
+import Avatar from "vue-boring-avatars";
+export default {
+  name: 'Identicon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Int8Array,
+      required: true
+    },
+  },
+  components: {
+    Avatar
+  }
+}
+</script>
